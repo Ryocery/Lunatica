fd1fc77b9280f4712f28b631f55a3cc704a8d9ba
Ryocery <youri.haentjens@gmail.com>
2024-03-11 19:59:58 +0100

    Setting website back to old state

---
+++ assets/scripts/main.js
@@ -0,0 +1,47 @@
+// Main Logic //
+
+let selectedGame
+let selectedHover
+
+function disableMainSelect() {
+  let games = document.getElementsByClassName("game")
+  for (let i = 0; i < games.length; i++) {
+    games[i].onclick = null;
+  }
+}
+
+function enableMainSelect() {
+  let games = document.getElementsByClassName("game")
+
+  selectedGame.revert()
+  selectedHover.revert()
+
+  for (let i = 0; i < games.length; i++) {
+    games[i].onclick = function () { mainSelect(document.getElementById(games[i].id)); };
+  }
+}
+
+async function mainSelect(item) {
+  selectedGame = gsap.to(item, {
+    duration:0,
+    backgroundColor:"white",
+    color:"black",
+  });
+
+  selectedHover = gsap.set(".game:hover img", {
+    filter: "brightness(0)",
+  });
+
+  disableMainSelect()
+  await delay(3000)
+  window.location.replace("./games/" + item.id + ".html")
+}
+
+// - - - - - - - //
+// - - Extra - - //
+// - - - - - - - //
+
+// For use in Async functions, delays next line of code by (time) milliseconds.
+function delay(time) {
+  return new Promise(resolve => setTimeout(resolve, time));
+}
